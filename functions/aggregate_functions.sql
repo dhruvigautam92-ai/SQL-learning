97f31716ded314c8f3f4a9810c8e562e2d43806d
dhruvigautam92-ai <dhruvigautam92@gmail.com>
2026-02-04 11:30:53 +0530

    Add aggregate functions SQL queries
    Added SQL queries for aggregate functions on students.

---
+++ functions/aggregate_functions.sql
@@ -0,0 +1,15 @@
+--1. COUNT()
+SELECT COUNT (*)
+FROM students;
+--2.SUM()
+SELECT SUM(marks)
+FROM students;
+--3.AVG()
+SELECT AVG(marks)
+FROM students;
+--4.MIN()
+SELECT MIN(marks)
+FROM students;
+--5.MAX()
+SELECT MAX(marks)
+FROM students;
