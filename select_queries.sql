79243d52c47567477b5e4937b4d3f48448295329
dhruvigautam92-ai <dhruvigautam92@gmail.com>
2026-02-05 10:40:37 +0530

    Add basic SELECT queries

---
+++ select_queries.sql
@@ -0,0 +1,6 @@
+--view all students
+SELECT * FROM students;
+--selectig students acc to a particular age
+SELECT * FROM students WHERE age>20;
+--only names and courses
+SELECT name, course FROM students;
