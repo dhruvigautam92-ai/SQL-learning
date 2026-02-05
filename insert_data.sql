af993471f4d811d9348f6ecc4d560747d5899fca
dhruvigautam92-ai <dhruvigautam92@gmail.com>
2026-02-05 10:31:05 +0530

    Add sample data to students table

---
+++ insert_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO students (id,name,age,course) VALUES
+(1, 'Amit', 20 , 'computer science'),
+(1, 'Neha', 21 , 'IT'),
+(1, 'Amit', 19 , 'Data Analytics')
+(1, 'Ritesh', 18 , 'web  development');
+
